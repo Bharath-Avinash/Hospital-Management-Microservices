3a180fa67dd1fe9ca7fbbbbefb780b591bf33258
Bharath Avinash <bharathavinash07@gmail.com>
2022-03-27 13:18:02 +0530

    Intial Commit

---
+++ Patient/src/test/java/com/example/demo/PatientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.repository.PatientRepository;
+import com.example.demo.service.PatientService;
+
+@SpringBootTest
+class PatientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+}
